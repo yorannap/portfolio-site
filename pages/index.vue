4e7b89e88d177d3880c1fdfb0b66dd19331ee62b
Yoran Nap <yoran.nap@gmail.com>
2021-05-20 20:24:27 +1200

    vh fix

--- pages/index.vue
+++ pages/index.vue
@@ -295,6 +295,12 @@ export default {
       repeat: true,
     });
     window.addEventListener("resize", this.loco);
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+      //console.log(vh);
+    });
   }
 }
 </script>
