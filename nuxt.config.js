be7aa06d6acd550f6f471d46c217aa87c80b0fb4
Yoran Nap <yoran.nap@gmail.com>
2021-05-20 21:34:37 +1200

    notch

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: "viewport", content: "width=device-width, initial-scale=1.0, viewport-fit=cover" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
