24e53629ae9ee99d0933cbe3d82a8d5ef8f52eb6
Yoran Nap <yoran.nap@gmail.com>
2021-09-15 17:47:51 +1200

    favicon and meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: "viewport", content: "width=device-width, initial-scale=1.0, viewport-fit=cover" }
+      { name: "viewport", content: "width=device-width, initial-scale=1.0, viewport-fit=cover" },
+      { hid: 'description', name: 'description', content: "Kia ora, I'm Yoran Nap, a designer working from Wellington, Aotearoa/New Zealand." },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
